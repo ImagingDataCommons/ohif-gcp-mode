d242e63cd3f63db8af60c503aa601335efea2ed5
Igor Octaviano <igoroctaviano@gmail.com>
2023-12-06 12:22:53 -0300

    Add merge server logic

--- src/index.js
+++ src/index.js
@@ -1,66 +1,66 @@
-import { hotkeys } from '@ohif/core';
-import toolbarButtons from './toolbarButtons.js';
-import { id } from './id.js';
-import initToolGroups from './initToolGroups.js';
+import { hotkeys } from "@ohif/core";
+import toolbarButtons from "./toolbarButtons.js";
+import { id } from "./id.js";
+import initToolGroups from "./initToolGroups.js";
 
 // Allow this mode by excluding non-imaging modalities such as SR, SEG
 // Also, SM is not a simple imaging modalities, so exclude it.
-const NON_IMAGE_MODALITIES = ['SM', 'ECG', 'SR', 'SEG', 'RTSTRUCT'];
+const NON_IMAGE_MODALITIES = ["SM", "ECG", "SR", "SEG", "RTSTRUCT"];
 
 const ohif = {
-  layout: '@ohif/extension-default.layoutTemplateModule.viewerLayout',
-  sopClassHandler: '@ohif/extension-default.sopClassHandlerModule.stack',
-  thumbnailList: '@ohif/extension-default.panelModule.seriesList',
+  layout: "@ohif/extension-default.layoutTemplateModule.viewerLayout",
+  sopClassHandler: "@ohif/extension-default.sopClassHandlerModule.stack",
+  thumbnailList: "@ohif/extension-default.panelModule.seriesList",
 };
 
 const tracked = {
   measurements:
-    '@ohif/extension-measurement-tracking.panelModule.trackedMeasurements',
-  thumbnailList: '@ohif/extension-measurement-tracking.panelModule.seriesList',
+    "@ohif/extension-measurement-tracking.panelModule.trackedMeasurements",
+  thumbnailList: "@ohif/extension-measurement-tracking.panelModule.seriesList",
   viewport:
-    '@ohif/extension-measurement-tracking.viewportModule.cornerstone-tracked',
+    "@ohif/extension-measurement-tracking.viewportModule.cornerstone-tracked",
 };
 
 const dicomsr = {
   sopClassHandler:
-    '@ohif/extension-cornerstone-dicom-sr.sopClassHandlerModule.dicom-sr',
-  viewport: '@ohif/extension-cornerstone-dicom-sr.viewportModule.dicom-sr',
+    "@ohif/extension-cornerstone-dicom-sr.sopClassHandlerModule.dicom-sr",
+  viewport: "@ohif/extension-cornerstone-dicom-sr.viewportModule.dicom-sr",
 };
 
 const dicomvideo = {
   sopClassHandler:
-    '@ohif/extension-dicom-video.sopClassHandlerModule.dicom-video',
-  viewport: '@ohif/extension-dicom-video.viewportModule.dicom-video',
+    "@ohif/extension-dicom-video.sopClassHandlerModule.dicom-video",
+  viewport: "@ohif/extension-dicom-video.viewportModule.dicom-video",
 };
 
 const dicompdf = {
-  sopClassHandler: '@ohif/extension-dicom-pdf.sopClassHandlerModule.dicom-pdf',
-  viewport: '@ohif/extension-dicom-pdf.viewportModule.dicom-pdf',
+  sopClassHandler: "@ohif/extension-dicom-pdf.sopClassHandlerModule.dicom-pdf",
+  viewport: "@ohif/extension-dicom-pdf.viewportModule.dicom-pdf",
 };
 
 const dicomSeg = {
   sopClassHandler:
-    '@ohif/extension-cornerstone-dicom-seg.sopClassHandlerModule.dicom-seg',
-  viewport: '@ohif/extension-cornerstone-dicom-seg.viewportModule.dicom-seg',
-  panel: '@ohif/extension-cornerstone-dicom-seg.panelModule.panelSegmentation',
+    "@ohif/extension-cornerstone-dicom-seg.sopClassHandlerModule.dicom-seg",
+  viewport: "@ohif/extension-cornerstone-dicom-seg.viewportModule.dicom-seg",
+  panel: "@ohif/extension-cornerstone-dicom-seg.panelModule.panelSegmentation",
 };
 
 const dicomRt = {
-  viewport: '@ohif/extension-cornerstone-dicom-rt.viewportModule.dicom-rt',
+  viewport: "@ohif/extension-cornerstone-dicom-rt.viewportModule.dicom-rt",
   sopClassHandler:
-    '@ohif/extension-cornerstone-dicom-rt.sopClassHandlerModule.dicom-rt',
+    "@ohif/extension-cornerstone-dicom-rt.sopClassHandlerModule.dicom-rt",
 };
 
 const extensionDependencies = {
   // Can derive the versions at least process.env.from npm_package_version
-  '@ohif/extension-default': '^3.0.0',
-  '@ohif/extension-cornerstone': '^3.0.0',
-  '@ohif/extension-measurement-tracking': '^3.0.0',
-  '@ohif/extension-cornerstone-dicom-sr': '^3.0.0',
-  '@ohif/extension-cornerstone-dicom-seg': '^3.0.0',
-  '@ohif/extension-cornerstone-dicom-rt': '^3.0.0',
-  '@ohif/extension-dicom-pdf': '^3.0.1',
-  '@ohif/extension-dicom-video': '^3.0.1',
+  "@ohif/extension-default": "^3.0.0",
+  "@ohif/extension-cornerstone": "^3.0.0",
+  "@ohif/extension-measurement-tracking": "^3.0.0",
+  "@ohif/extension-cornerstone-dicom-sr": "^3.0.0",
+  "@ohif/extension-cornerstone-dicom-seg": "^3.0.0",
+  "@ohif/extension-cornerstone-dicom-rt": "^3.0.0",
+  "@ohif/extension-dicom-pdf": "^3.0.1",
+  "@ohif/extension-dicom-video": "^3.0.1",
 };
 
 function modeFactory() {
@@ -70,12 +70,35 @@ function modeFactory() {
     // We should not be.
     id,
     routeName:
-      'projects/:project/locations/:location/datasets/:dataset/dicomStores/:dicomStore/study/:StudyInstanceUIDs',
+      "projects/:project/locations/:location/datasets/:dataset/dicomStores/:dicomStore/study/:StudyInstanceUIDs",
     /**
      * Lifecycle hooks
      */
-    onModeInit: ({ extensionManager }) => {
-      extensionManager.setActiveDataSource('gcp-dicomweb');
+    onModeInit: ({ extensionManager, query }) => {
+      const primaryDataSourceName = "gcp-dicomweb";
+      const secondGoogleServer = query.get("secondGoogleServer");
+      if (secondGoogleServer) {
+        const dataSourceBasedOnURL = secondGoogleServer.includes("/dicomStores")
+          ? "gcp-dicomweb-2"
+          : "idc-dicomweb";
+        extensionManager.addDataSource(
+          {
+            sourceName: "merge",
+            namespace: "@ohif/extension-default.dataSourcesModule.merge",
+            configuration: {
+              name: "merge",
+              friendlyName: "Merge Data Source",
+              seriesMerge: {
+                dataSourceNames: [primaryDataSourceName, dataSourceBasedOnURL],
+                defaultDataSourceName: primaryDataSourceName,
+              },
+            },
+          },
+          { activate: true }
+        );
+      } else {
+        extensionManager.setActiveDataSource(primaryDataSourceName);
+      }
     },
     onModeEnter: ({ servicesManager, extensionManager, commandsManager }) => {
       const {
@@ -95,16 +118,16 @@ function modeFactory() {
 
       const activateTool = () => {
         toolbarService.recordInteraction({
-          groupId: 'WindowLevel',
-          itemId: 'WindowLevel',
-          interactionType: 'tool',
+          groupId: "WindowLevel",
+          itemId: "WindowLevel",
+          interactionType: "tool",
           commands: [
             {
-              commandName: 'setToolActive',
+              commandName: "setToolActive",
               commandOptions: {
-                toolName: 'WindowLevel',
+                toolName: "WindowLevel",
               },
-              context: 'CORNERSTONE',
+              context: "CORNERSTONE",
             },
           ],
         });
@@ -123,16 +146,16 @@ function modeFactory() {
 
       toolbarService.init(extensionManager);
       toolbarService.addButtons(toolbarButtons);
-      toolbarService.createButtonSection('primary', [
-        'MeasurementTools',
-        'Zoom',
-        'WindowLevel',
-        'Pan',
-        'Capture',
-        'Layout',
-        'MPR',
-        'Crosshairs',
-        'MoreTools',
+      toolbarService.createButtonSection("primary", [
+        "MeasurementTools",
+        "Zoom",
+        "WindowLevel",
+        "Pan",
+        "Capture",
+        "Layout",
+        "MPR",
+        "Crosshairs",
+        "MoreTools",
       ]);
 
       // // ActivatePanel event trigger for when a segmentation or measurement is added.
@@ -166,7 +189,7 @@ function modeFactory() {
         cornerstoneViewportService,
       } = servicesManager.services;
 
-      _activatePanelTriggersSubscriptions.forEach(sub => sub.unsubscribe());
+      _activatePanelTriggersSubscriptions.forEach((sub) => sub.unsubscribe());
       _activatePanelTriggersSubscriptions = [];
 
       toolGroupService.destroy();
@@ -179,17 +202,17 @@ function modeFactory() {
       series: [],
     },
 
-    isValidMode: function({ modalities }) {
-      const modalities_list = modalities.split('\\');
+    isValidMode: function ({ modalities }) {
+      const modalities_list = modalities.split("\\");
 
       // Exclude non-image modalities
       return !!modalities_list.filter(
-        modality => NON_IMAGE_MODALITIES.indexOf(modality) === -1
+        (modality) => NON_IMAGE_MODALITIES.indexOf(modality) === -1
       ).length;
     },
     routes: [
       {
-        path: 'longitudinal',
+        path: "longitudinal",
         /*init: ({ servicesManager, extensionManager }) => {
           //defaultViewerRouteInit
         },*/
@@ -233,7 +256,7 @@ function modeFactory() {
     ],
     extensions: extensionDependencies,
     // Default protocol gets self-registered by default in the init
-    hangingProtocol: 'default',
+    hangingProtocol: "default",
     // Order is important in sop class handlers when two handlers both use
     // the same sop class under different situations.  In that case, the more
     // general handler needs to come last.  For this case, the dicomvideo must
