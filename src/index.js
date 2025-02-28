988d46b4f214daaab612bcdf448c08a9cb81895b
Igor Octaviano <igoroctaviano@gmail.com>
2025-02-28 12:37:25 -0300

    Update to OHIF 3.9.3

--- src/index.js
+++ src/index.js
@@ -1,78 +1,78 @@
-import { hotkeys } from "@ohif/core";
-import i18n from "i18next";
-
-import { id } from "./id";
-import initToolGroups from "./initToolGroups";
-import toolbarButtons from "./toolbarButtons";
-import moreTools from "./moreTools";
-import onModeInit from "./onModeInit";
-import routeName from "./routeName";
+import { hotkeys } from '@ohif/core';
+import i18n from 'i18next';
+import { id } from './id';
+import initToolGroups from './initToolGroups';
+import toolbarButtons from './toolbarButtons';
+import moreTools from './moreTools';
+import { performCustomizations } from './customizations';
+import onModeInit from './onModeInit';
+import routeName from './routeName';
 
 // Allow this mode by excluding non-imaging modalities such as SR, SEG
 // Also, SM is not a simple imaging modalities, so exclude it.
-const NON_IMAGE_MODALITIES = ["SM", "ECG", "SEG", "RTSTRUCT"];
+const NON_IMAGE_MODALITIES = ['ECG', 'SEG', 'RTSTRUCT', 'RTPLAN', 'PR'];
 
 const ohif = {
-  layout: "@ohif/extension-default.layoutTemplateModule.viewerLayout",
-  sopClassHandler: "@ohif/extension-default.sopClassHandlerModule.stack",
-  thumbnailList: "@ohif/extension-default.panelModule.seriesList",
+  layout: '@ohif/extension-default.layoutTemplateModule.viewerLayout',
+  sopClassHandler: '@ohif/extension-default.sopClassHandlerModule.stack',
+  thumbnailList: '@ohif/extension-default.panelModule.seriesList',
+  wsiSopClassHandler:
+    '@ohif/extension-cornerstone.sopClassHandlerModule.DicomMicroscopySopClassHandler',
+};
+
+const cornerstone = {
+  measurements: '@ohif/extension-cornerstone.panelModule.panelMeasurement',
+  segmentation: '@ohif/extension-cornerstone.panelModule.panelSegmentation',
 };
 
 const tracked = {
-  measurements:
-    "@ohif/extension-measurement-tracking.panelModule.trackedMeasurements",
-  thumbnailList: "@ohif/extension-measurement-tracking.panelModule.seriesList",
-  viewport:
-    "@ohif/extension-measurement-tracking.viewportModule.cornerstone-tracked",
+  measurements: '@ohif/extension-measurement-tracking.panelModule.trackedMeasurements',
+  thumbnailList: '@ohif/extension-measurement-tracking.panelModule.seriesList',
+  viewport: '@ohif/extension-measurement-tracking.viewportModule.cornerstone-tracked',
 };
 
 const dicomsr = {
-  sopClassHandler:
-    "@ohif/extension-cornerstone-dicom-sr.sopClassHandlerModule.dicom-sr",
-  viewport: "@ohif/extension-cornerstone-dicom-sr.viewportModule.dicom-sr",
+  sopClassHandler: '@ohif/extension-cornerstone-dicom-sr.sopClassHandlerModule.dicom-sr',
+  sopClassHandler3D: '@ohif/extension-cornerstone-dicom-sr.sopClassHandlerModule.dicom-sr-3d',
+  viewport: '@ohif/extension-cornerstone-dicom-sr.viewportModule.dicom-sr',
 };
 
 const dicomvideo = {
-  sopClassHandler:
-    "@ohif/extension-dicom-video.sopClassHandlerModule.dicom-video",
-  viewport: "@ohif/extension-dicom-video.viewportModule.dicom-video",
+  sopClassHandler: '@ohif/extension-dicom-video.sopClassHandlerModule.dicom-video',
+  viewport: '@ohif/extension-dicom-video.viewportModule.dicom-video',
 };
 
 const dicompdf = {
-  sopClassHandler: "@ohif/extension-dicom-pdf.sopClassHandlerModule.dicom-pdf",
-  viewport: "@ohif/extension-dicom-pdf.viewportModule.dicom-pdf",
+  sopClassHandler: '@ohif/extension-dicom-pdf.sopClassHandlerModule.dicom-pdf',
+  viewport: '@ohif/extension-dicom-pdf.viewportModule.dicom-pdf',
 };
 
 const dicomSeg = {
-  sopClassHandler:
-    "@ohif/extension-cornerstone-dicom-seg.sopClassHandlerModule.dicom-seg",
-  viewport: "@ohif/extension-cornerstone-dicom-seg.viewportModule.dicom-seg",
-  panel: "@ohif/extension-cornerstone-dicom-seg.panelModule.panelSegmentation",
+  sopClassHandler: '@ohif/extension-cornerstone-dicom-seg.sopClassHandlerModule.dicom-seg',
+  viewport: '@ohif/extension-cornerstone-dicom-seg.viewportModule.dicom-seg',
 };
 
 const dicomPmap = {
-  sopClassHandler:
-    "@ohif/extension-cornerstone-dicom-pmap.sopClassHandlerModule.dicom-pmap",
-  viewport: "@ohif/extension-cornerstone-dicom-pmap.viewportModule.dicom-pmap",
+  sopClassHandler: '@ohif/extension-cornerstone-dicom-pmap.sopClassHandlerModule.dicom-pmap',
+  viewport: '@ohif/extension-cornerstone-dicom-pmap.viewportModule.dicom-pmap',
 };
 
 const dicomRT = {
-  viewport: "@ohif/extension-cornerstone-dicom-rt.viewportModule.dicom-rt",
-  sopClassHandler:
-    "@ohif/extension-cornerstone-dicom-rt.sopClassHandlerModule.dicom-rt",
+  viewport: '@ohif/extension-cornerstone-dicom-rt.viewportModule.dicom-rt',
+  sopClassHandler: '@ohif/extension-cornerstone-dicom-rt.sopClassHandlerModule.dicom-rt',
 };
 
 const extensionDependencies = {
   // Can derive the versions at least process.env.from npm_package_version
-  "@ohif/extension-default": "^3.0.0",
-  "@ohif/extension-cornerstone": "^3.0.0",
-  "@ohif/extension-measurement-tracking": "^3.0.0",
-  "@ohif/extension-cornerstone-dicom-sr": "^3.0.0",
-  "@ohif/extension-cornerstone-dicom-seg": "^3.0.0",
-  "@ohif/extension-cornerstone-dicom-pmap": "^3.0.0",
-  "@ohif/extension-cornerstone-dicom-rt": "^3.0.0",
-  "@ohif/extension-dicom-pdf": "^3.0.1",
-  "@ohif/extension-dicom-video": "^3.0.1",
+  '@ohif/extension-default': '^3.0.0',
+  '@ohif/extension-cornerstone': '^3.0.0',
+  '@ohif/extension-measurement-tracking': '^3.0.0',
+  '@ohif/extension-cornerstone-dicom-sr': '^3.0.0',
+  '@ohif/extension-cornerstone-dicom-seg': '^3.0.0',
+  '@ohif/extension-cornerstone-dicom-pmap': '^3.0.0',
+  '@ohif/extension-cornerstone-dicom-rt': '^3.0.0',
+  '@ohif/extension-dicom-pdf': '^3.0.1',
+  '@ohif/extension-dicom-video': '^3.0.1',
 };
 
 function modeFactory({ modeConfiguration }) {
@@ -82,88 +82,60 @@ function modeFactory({ modeConfiguration }) {
     // We should not be.
     id,
     routeName,
-    displayName: i18n.t("Modes:Basic Viewer"),
+    displayName: i18n.t('Modes:Basic Viewer'),
     /**
      * Lifecycle hooks
      */
-    onModeInit: onModeInit,
-    onModeEnter: function ({
-      servicesManager,
-      extensionManager,
-      commandsManager,
-    }: withAppTypes) {
+    onModeInit,
+    onModeEnter: function ({ servicesManager, extensionManager, commandsManager }: withAppTypes) {
       const {
         measurementService,
         toolbarService,
         toolGroupService,
         customizationService,
+        panelService,
+        segmentationService,
       } = servicesManager.services;
 
       measurementService.clearMeasurements();
 
-      // customizationService.addModeCustomizations([
-      //   {
-      //     id: 'measurementLabels',
-      //     labelOnMeasure: true,
-      //     exclusive: true,
-      //     items: [
-      //       { value: 'Head', label: 'Head' },
-      //       { value: 'Shoulder', label: 'Shoulder' },
-      //       { value: 'Knee', label: 'Knee' },
-      //       { value: 'Toe', label: 'Toe' },
-      //     ],
-      //   },
-      // ]);
+      performCustomizations(customizationService);
 
       // Init Default and SR ToolGroups
-      initToolGroups(
-        extensionManager,
-        toolGroupService,
-        commandsManager,
-        this.labelConfig
-      );
+      initToolGroups(extensionManager, toolGroupService, commandsManager, this.labelConfig);
 
       toolbarService.addButtons([...toolbarButtons, ...moreTools]);
-      toolbarService.createButtonSection("primary", [
-        "MeasurementTools",
-        "Zoom",
-        "Pan",
-        "TrackballRotate",
-        "WindowLevel",
-        "Capture",
-        "Layout",
-        "Crosshairs",
-        "MoreTools",
-      ]);
-
-      customizationService.addModeCustomizations([
-        {
-          id: "segmentation.panel",
-          disableEditing: true,
-        },
+      toolbarService.createButtonSection('primary', [
+        'MeasurementTools',
+        'Zoom',
+        'Pan',
+        'TrackballRotate',
+        'WindowLevel',
+        'Capture',
+        'Layout',
+        'Crosshairs',
+        'MoreTools',
       ]);
 
       // // ActivatePanel event trigger for when a segmentation or measurement is added.
       // // Do not force activation so as to respect the state the user may have left the UI in.
-      // _activatePanelTriggersSubscriptions = [
-      //   ...panelService.addActivatePanelTriggers(dicomSeg.panel, [
-      //     {
-      //       sourcePubSubService: segmentationService,
-      //       sourceEvents: [
-      //         segmentationService.EVENTS.SEGMENTATION_PIXEL_DATA_CREATED,
-      //       ],
-      //     },
-      //   ]),
-      //   ...panelService.addActivatePanelTriggers(tracked.measurements, [
-      //     {
-      //       sourcePubSubService: measurementService,
-      //       sourceEvents: [
-      //         measurementService.EVENTS.MEASUREMENT_ADDED,
-      //         measurementService.EVENTS.RAW_MEASUREMENT_ADDED,
-      //       ],
-      //     },
-      //   ]),
-      // ];
+      _activatePanelTriggersSubscriptions = [
+        ...panelService.addActivatePanelTriggers(cornerstone.segmentation, [
+          {
+            sourcePubSubService: segmentationService,
+            sourceEvents: [segmentationService.EVENTS.SEGMENTATION_ADDED],
+          },
+        ]),
+        ...panelService.addActivatePanelTriggers(tracked.measurements, [
+          {
+            sourcePubSubService: measurementService,
+            sourceEvents: [
+              measurementService.EVENTS.MEASUREMENT_ADDED,
+              measurementService.EVENTS.RAW_MEASUREMENT_ADDED,
+            ],
+          },
+        ]),
+      ];
     },
     onModeExit: ({ servicesManager }: withAppTypes) => {
       const {
@@ -175,7 +147,7 @@ function modeFactory({ modeConfiguration }) {
         uiModalService,
       } = servicesManager.services;
 
-      _activatePanelTriggersSubscriptions.forEach((sub) => sub.unsubscribe());
+      _activatePanelTriggersSubscriptions.forEach(sub => sub.unsubscribe());
       _activatePanelTriggersSubscriptions = [];
 
       uiDialogService.dismissAll();
@@ -191,20 +163,19 @@ function modeFactory({ modeConfiguration }) {
     },
 
     isValidMode: function ({ modalities }) {
-      const modalities_list = modalities.split("\\");
+      const modalities_list = modalities.split('\\');
 
       // Exclude non-image modalities
       return {
-        valid: !!modalities_list.filter(
-          (modality) => NON_IMAGE_MODALITIES.indexOf(modality) === -1
-        ).length,
+        valid: !!modalities_list.filter(modality => NON_IMAGE_MODALITIES.indexOf(modality) === -1)
+          .length,
         description:
-          "The mode does not support studies that ONLY include the following modalities: SM, ECG, SEG, RTSTRUCT",
+          'The mode does not support studies that ONLY include the following modalities: SM, ECG, SEG, RTSTRUCT',
       };
     },
     routes: [
       {
-        path: "longitudinal",
+        path: 'longitudinal',
         /*init: ({ servicesManager, extensionManager }) => {
           //defaultViewerRouteInit
         },*/
@@ -213,20 +184,21 @@ function modeFactory({ modeConfiguration }) {
             id: ohif.layout,
             props: {
               leftPanels: [tracked.thumbnailList],
-              rightPanels: [dicomSeg.panel, tracked.measurements],
+              rightPanels: [cornerstone.segmentation, tracked.measurements],
               rightPanelClosed: true,
               viewports: [
                 {
                   namespace: tracked.viewport,
-                  displaySetsToDisplay: [ohif.sopClassHandler],
+                  displaySetsToDisplay: [
+                    ohif.sopClassHandler,
+                    dicomvideo.sopClassHandler,
+                    dicomsr.sopClassHandler3D,
+                    ohif.wsiSopClassHandler,
+                  ],
                 },
                 {
                   namespace: dicomsr.viewport,
                   displaySetsToDisplay: [dicomsr.sopClassHandler],
-                },
-                {
-                  namespace: dicomvideo.viewport,
-                  displaySetsToDisplay: [dicomvideo.sopClassHandler],
                 },
                 {
                   namespace: dicompdf.viewport,
@@ -252,7 +224,7 @@ function modeFactory({ modeConfiguration }) {
     ],
     extensions: extensionDependencies,
     // Default protocol gets self-registered by default in the init
-    hangingProtocol: "default",
+    hangingProtocol: 'default',
     // Order is important in sop class handlers when two handlers both use
     // the same sop class under different situations.  In that case, the more
     // general handler needs to come last.  For this case, the dicomvideo must
@@ -262,7 +234,9 @@ function modeFactory({ modeConfiguration }) {
       dicomSeg.sopClassHandler,
       dicomPmap.sopClassHandler,
       ohif.sopClassHandler,
+      ohif.wsiSopClassHandler,
       dicompdf.sopClassHandler,
+      dicomsr.sopClassHandler3D,
       dicomsr.sopClassHandler,
       dicomRT.sopClassHandler,
     ],
